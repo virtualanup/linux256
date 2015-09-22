994377d811e0b3e68e0590dd40bfdde7a4d09f6c
John Criswell <jtcriswel@gmail.com>
2015-09-22 15:53:55 -0400

    Enhanced the runqemu script so that it can run the default kernel.

--- runqemu.sh
+++ runqemu.sh
@@ -3,4 +3,11 @@
 #
 # First argument: Name of kernel bzImage to execute
 #
-./bin/qemu-system-x86_64 -m 64M -L ./qemu/share/qemu/ -nographic -kernel $1 -hda $HOME/mp2/openwrt-15.05-x86-generic-combined-ext4.img -append "root=PARTUUID=076b2e55-02 rootfstype=ext4 rootwait console=tty0 console=ttyS0,38400n8 noinitrd"
+if [ -z $1 ]
+then
+	echo "Running QEMU with installed kernel"
+	./install/bin/qemu-system-x86_64 -m 64M -L ./install/qemu/share/qemu/ -nographic -hda ./openwrt-15.05-x86-generic-combined-ext4.img
+else
+	echo "Running QEMU with your kernel at $1"
+	./install/bin/qemu-system-x86_64 -m 64M -L ./install/qemu/share/qemu/ -nographic -kernel $1 -hda ./openwrt-15.05-x86-generic-combined-ext4.img -append "root=PARTUUID=076b2e55-02 rootfstype=ext4 rootwait console=tty0 console=ttyS0,38400n8 noinitrd"
+fi
